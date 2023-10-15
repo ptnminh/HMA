87df9c9a013d9d6594fc3daa7af8a0315bb2b542
ptnminh <minhphan113@gmail.com>
2023-10-15 19:27:53 +0700

    feat: auth & mail service

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('API docs')
+    .addTag('users')
+    .addTag('tasks')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(new ConfigService().get('PORT'));
 }
 bootstrap();
