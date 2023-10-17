0a5ffeaf6ee0ba417c8d76b10961d8f0c81b5194
ptnminh <minhphan113@gmail.com>
2023-10-17 23:34:48 +0700

    feat(gateway): add custom handling error when receive bad input request

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -2,6 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +18,26 @@ async function bootstrap() {
     .addTag('users')
     .setVersion('1.0')
     .build();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const result = errors.map((error) => ({
+          property: error.property,
+          message: error.constraints[Object.keys(error.constraints)[0]],
+        }));
+        throw new HttpException(
+          {
+            message: result[0].message,
+            data: null,
+            errors: true,
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+      },
+      transform: true,
+      stopAtFirstError: true,
+    }),
+  );
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api/v1/swagger', app, document);
   await app.listen(configService.get('PORT'));
