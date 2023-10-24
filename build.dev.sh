4699251e57fb72bbe5159acc165144418bc2a8a6
ptnminh <minhphan113@gmail.com>
2023-10-24 23:51:27 +0700

    feat: update build stage

--- build.dev.sh
+++ build.dev.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+echo "Script executed from: ${PWD}"
 
+BASEDIR=$(dirname $0)
+echo "Script location: ${BASEDIR}"
 
 
 [ ! "$(docker ps -a | grep postgres)" ] && docker run -d -p 5555:5432 --network=some-network -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=123 -e POSTGRES_DB=hma --name hma_db \
-    -v db:/var/lib/postgresql/data postgres
+    -v ${PWD}/db:/var/lib/postgresql/data postgres
 
 cd shared/libs/prisma 
 
