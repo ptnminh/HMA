ce702d4728928d2430003bd6c76208e567ed5cfd
ptnminh <minhphan113@gmail.com>
2023-10-24 23:45:22 +0700

    feat(auth): handle google oauth

--- apps/auth/src/auth/auth.controller.ts
+++ apps/auth/src/auth/auth.controller.ts
@@ -3,10 +3,17 @@ import { AuthService } from './auth.service';
 import { ClientProxy, MessagePattern } from '@nestjs/microservices';
 import { AuthCommand } from './command';
 import { RegisterDto } from './dto/create-user.dto';
-import { EVENTS, ROLES, comparePassword, hashPassword } from 'src/shared';
+import {
+  EVENTS,
+  PROVIDERS,
+  ROLES,
+  comparePassword,
+  hashPassword,
+} from 'src/shared';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { lastValueFrom } from 'rxjs';
+import { isEmpty } from 'lodash';
 
 @Controller('auth')
 export class AuthController {
@@ -204,6 +211,75 @@ export class AuthController {
         status: HttpStatus.OK,
         message: 'Gửi mail thành công',
         link: linkComfirm,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Internal server error',
+      };
+    }
+  }
+
+  @MessagePattern(AuthCommand.USER_OAUTH_LOGIN)
+  async loginWithGoogle(data: any) {
+    try {
+      const { user: oauthUser } = data;
+      let userId: string = '';
+      let user: any;
+      const exUser = await this.authService.findUserByEmail(oauthUser.email);
+      if (exUser) {
+        // if this user has no account then create one else do nothing
+        const account = await this.authService.findAccountByUserId(
+          exUser.id,
+          PROVIDERS.GOOGLE,
+        );
+        if (isEmpty(account)) {
+          // link user to provider account
+          await this.authService.createAccount({
+            userId: exUser.id,
+            provider: PROVIDERS.GOOGLE,
+            avatar: oauthUser.picture,
+          });
+        }
+
+        userId = exUser.id;
+        user = exUser;
+      } else {
+        const createdUser = await this.authService.signUpByEmail({
+          email: oauthUser.email,
+          password: oauthUser.email,
+          firstName: oauthUser.firstName,
+          lastName: oauthUser.lastName,
+          emailVerified: true,
+        });
+        await this.authService.createAccount({
+          userId: createdUser.id,
+          provider: 'google',
+          avatar: oauthUser.picture,
+        });
+        userId = createdUser.id;
+        user = createdUser;
+      }
+      const jwtSercret = this.configService.get<string>('JWT_SECRET_KEY');
+      const registerToken = await this.jwtService.signAsync(
+        {
+          id: userId,
+        },
+        {
+          secret: jwtSercret,
+          expiresIn: '30d',
+        },
+      );
+      return {
+        message: 'Create user successfully',
+        status: HttpStatus.CREATED,
+        user: {
+          ...user,
+          emailVerified: true,
+          role: user.role.name,
+        },
+        token: registerToken,
       };
     } catch (error) {
       console.log(error);
--- apps/auth/src/shared/libs/common/constant/index.ts
+++ apps/auth/src/shared/libs/common/constant/index.ts
@@ -1,2 +1,3 @@
 export * from './roles';
 export * from './events';
+export * from './providers';
