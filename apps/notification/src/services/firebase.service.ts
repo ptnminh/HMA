c18f48104febb1589bb580f00082f495a9a93c74
ptnminh <minhphan113@gmail.com>
2023-12-16 01:36:02 +0700

    feat: update flow init firebase

--- apps/notification/src/services/firebase.service.ts
+++ apps/notification/src/services/firebase.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import * as firebase from 'firebase-admin';
-import * as FCM from 'fcm-node';
-import config from './firebase.config.json';
+// import config from './firebase.config.json';
 @Injectable()
 export class FirebaseService {
   constructor() {
-    firebase.initializeApp({
-      credential: firebase.credential.cert({
-        projectId: config.project_id,
-        clientEmail: config.client_email,
-        privateKey: config.private_key,
-      }),
-    });
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBUjR_LpKzbeLaBANVXDN84BDLPLRn6VhM',
+      authDomain: 'clinus-1d1d1.firebaseapp.com',
+      databaseURL:
+        'https://clinus-1d1d1-default-rtdb.asia-southeast1.firebasedatabase.app',
+      projectId: 'clinus-1d1d1',
+      storageBucket: 'clinus-1d1d1.appspot.com',
+      messagingSenderId: '698964272341',
+      appId: '1:698964272341:web:f8e27c1489c69dbf6cee5c',
+      measurementId: 'G-13Z9189280',
+    };
+    // firebase.initializeApp({
+    //   credential: firebase.credential.cert({
+    //     projectId: config.project_id,
+    //     clientEmail: config.client_email,
+    //     privateKey: config.private_key,
+    //   }),
+    // });
+    firebase.initializeApp(firebaseConfig);
   }
 
   public sendMessage(
