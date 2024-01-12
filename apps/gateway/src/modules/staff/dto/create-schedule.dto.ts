0569a4a4b2d1e6a73155025ae2f250490d183ef0
nsonbao261 <91799283+nsonbao261@users.noreply.github.com>
2024-01-12 22:30:25 +0700

    fix return dateTime

--- apps/gateway/src/modules/staff/dto/create-schedule.dto.ts
+++ apps/gateway/src/modules/staff/dto/create-schedule.dto.ts
@@ -3,13 +3,13 @@ import { IsDateString, IsNotEmpty, IsString } from "class-validator";
 
 export class CreeateScheduleDto {
 
-    @ApiProperty({example: '2024-10-01T07:30:00'})
+    @ApiProperty({example: '2024-10-01T07:30:00.000Z'})
     @IsDateString()
     @IsString()
     @IsNotEmpty()
     startTime: string;
 
-    @ApiProperty({example: '2024-10-01T16:00:00'})
+    @ApiProperty({example: '2024-10-01T16:00:00.000Z'})
     @IsDateString()
     @IsString()
     @IsNotEmpty()
